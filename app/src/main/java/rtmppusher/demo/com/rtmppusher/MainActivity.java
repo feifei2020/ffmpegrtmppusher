006831d919669808056f56014f19ddd5351414db
feifei2020 <48463008+feifei2020@users.noreply.github.com>
2019-03-31 14:28:26 +0800

    v1

--- app/src/main/java/rtmppusher/demo/com/rtmppusher/MainActivity.java
+++ app/src/main/java/rtmppusher/demo/com/rtmppusher/MainActivity.java
@@ -1,35 +1,53 @@
 package rtmppusher.demo.com.rtmppusher;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 
+import butterknife.BindView;
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.util.Rotation;
 import rtmppusher.demo.com.rtmppusher.utils.pusher.AudioSource;
 import rtmppusher.demo.com.rtmppusher.utils.pusher.VideoSource;
 
 public class MainActivity extends Activity {
-
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
-    AudioSource audioSource;
-    VideoSource videoSource;
-    String videoUrl = "rtmp://192.168.0.105/live/1234";
+    @BindView(R.id.glSurfaceView)
+    GLSurfaceView glSurfaceView;
+
+    private AudioSource audioSource;
+    private VideoSource videoSource;
+    private String videoUrl = "rtmp://192.168.0.105/live/1234";
+    private GPUImage gpuImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //GLSurfaceView预览
+        glSurfaceView = findViewById(R.id.glSurfaceView);
+        //音频封装
         audioSource = new AudioSource();
-        audioSource.setRecordCallback(this::onRecordFrame);
+        audioSource.setRecordCallback(this::onRecordFrame); //回调
+        //摄像头封装
         videoSource = new VideoSource();
-        videoSource.setPreviewCallback(this::onPreviewFrame);
+        videoSource.setPreviewCallback(this::onPreviewFrame); //回调
+        //添加gpu美颜
+        gpuImage = new GPUImage(this);
+        gpuImage.setRotation(Rotation.ROTATION_270, false, true); //镜像
+        gpuImage.setGLSurfaceView(glSurfaceView);
+        //推流初始化
         init();
-        startPush(videoUrl);
+        startPush(videoUrl); //开始推
+
         //权限申请
         AndPermission.with(this)
                 .permission(Permission.Group.CAMERA)
@@ -64,11 +82,18 @@ public class MainActivity extends Activity {
 
     public void onRecordFrame(Object obj) {
         byte[] data = (byte[])obj;
+        //音频推流
         pushAudio(data, data.length, 0);
     }
 
     public void onPreviewFrame(Object obj) {
         byte[] data = (byte[])obj;
+        //gpu美颜色显示
+        if (glSurfaceView != null && gpuImage != null) {
+            gpuImage.updatePreviewFrame(data, 640, 480);
+            gpuImage.requestRender();
+        }
+        //视频推流
         pushVideo(data, data.length, 0);
     }
 
